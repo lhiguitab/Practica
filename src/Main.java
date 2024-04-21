1340d2cf5d134740a843d9ed12440844d42fd194
Ang3l1485 <maortizp@eafit.edu.co>
2024-04-21 18:09:22 -0500

    Tareita sobre pull request

--- src/Main.java
+++ src/Main.java
@@ -52,5 +52,6 @@ public class Main {
     static void Minombre(){
       System.out.println("mi nombre es Miguel Angel Ortiz Puerta");
       System.out.println("soy del grupo de lenguajes de 6 a 9");
+      System.out.println("tercera prueba de pull request");
     }
 }
